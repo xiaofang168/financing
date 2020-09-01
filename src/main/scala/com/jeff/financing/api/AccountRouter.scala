9a842d7d384631d65c2953cfacf437a95b56c226
fangjie <fangjie@etouch.cn>
2020-09-01 16:11:53 +0800

    简化返回Future

--- src/main/scala/com/jeff/financing/api/AccountRouter.scala
+++ src/main/scala/com/jeff/financing/api/AccountRouter.scala
@@ -4,16 +4,11 @@ import akka.http.scaladsl.server.Directives._
 import com.jeff.financing.entity.AccountJsonSupport._
 import com.jeff.financing.service.AccountService
 
-import scala.util.{Failure, Success}
-
 object AccountRouter extends AccountService {
 
   val route =
     path("accounts" / Remaining) { id =>
-      onComplete(get(id)) {
-        case Success(value) => complete(value)
-        case Failure(ex) => complete(s"An error occurred: ${ex.getMessage}")
-      }
+      complete(get(id))
     }
 
 }
