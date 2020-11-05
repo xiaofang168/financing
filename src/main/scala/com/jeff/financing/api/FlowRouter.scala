b403e0c503da6cb7731610ff8c03e9348328f936
fangjie <fangjie@etouch.cn>
2020-11-05 17:03:42 +0800

    支持跨越

--- src/main/scala/com/jeff/financing/api/FlowRouter.scala
+++ src/main/scala/com/jeff/financing/api/FlowRouter.scala
@@ -4,13 +4,13 @@ import akka.http.scaladsl.server.Directives._
 import com.jeff.financing.dto.FlowItemJsonSupport._
 import com.jeff.financing.service.FlowService
 
-object FlowRouter extends FlowService {
+object FlowRouter extends FlowService with CORSHandler {
 
   val route =
     path("flows" / Remaining) { id =>
       complete(get(id))
     } ~ path("flows") {
-      complete(list())
+      corsHandler(complete(list()))
     }
 
 }
