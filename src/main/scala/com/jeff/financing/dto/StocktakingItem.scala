e3ffaee8880b469254b94b6143b2320422e4d49c
fangjie <fangjie@etouch.cn>
2020-12-02 20:18:47 +0800

    增加targetId

--- src/main/scala/com/jeff/financing/dto/StocktakingItem.scala
+++ src/main/scala/com/jeff/financing/dto/StocktakingItem.scala
@@ -3,8 +3,8 @@ package com.jeff.financing.dto
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import com.jeff.financing.internal.LowerCaseWithUnderscores
 
-case class StocktakingItem(id: String, date: String, amount: BigDecimal, comment: Option[String])
+case class StocktakingItem(targetId: String, id: String, date: String, amount: BigDecimal, comment: Option[String])
 
 object StocktakingItemJsonSupport extends LowerCaseWithUnderscores with SprayJsonSupport {
-  implicit val stocktakingItemFormats = jsonFormat4(StocktakingItem)
+  implicit val stocktakingItemFormats = jsonFormat5(StocktakingItem)
 }
