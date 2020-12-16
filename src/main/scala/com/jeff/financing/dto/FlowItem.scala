3b62c3da784153b89595ab656331434814a34eaa
fangjie <fangjie@etouch.cn>
2020-12-16 11:42:54 +0800

    增加盘点金额字段

--- src/main/scala/com/jeff/financing/dto/FlowItem.scala
+++ src/main/scala/com/jeff/financing/dto/FlowItem.scala
@@ -6,8 +6,9 @@ import com.jeff.financing.internal.LowerCaseWithUnderscores
 case class FlowItem(id: String, platform: Option[String], category: String, categoryDesc: String,
                     state: Int, stateDesc: String, amount: BigDecimal, rate: Option[BigDecimal],
                     dailyIncome: Option[BigDecimal], days: Option[Int], allIncome: Option[BigDecimal],
-                    target: String, startTime: Option[String], endTime: Option[String], createTime: String)
+                    stocktakingAmount: BigDecimal, target: String, startTime: Option[String],
+                    endTime: Option[String], createTime: String)
 
 object FlowItemJsonSupport extends LowerCaseWithUnderscores with SprayJsonSupport {
-  implicit val flowItemFormats = jsonFormat15(FlowItem)
+  implicit val flowItemFormats = jsonFormat16(FlowItem)
 }
