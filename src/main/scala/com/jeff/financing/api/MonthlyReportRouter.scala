80f00a5fe0009423ed1e44e01314949e30bb4681
fangjie <fangjie@etouch.cn>
2021-01-07 18:15:33 +0800

    增加月报详情和生成月报的接口

--- src/main/scala/com/jeff/financing/api/MonthlyReportRouter.scala
+++ src/main/scala/com/jeff/financing/api/MonthlyReportRouter.scala
@@ -4,6 +4,8 @@ import akka.http.scaladsl.server.Directives._
 import com.jeff.financing.dto.MonthlyReportItemJsonSupport._
 import com.jeff.financing.service.MonthlyReportService
 
+import scala.util.{Failure, Success}
+
 object MonthlyReportRouter {
 
   val service = new MonthlyReportService {}
@@ -13,6 +15,23 @@ object MonthlyReportRouter {
         parameters("start_date".as[Int], "end_date".as[Int]) { (startDate, endDate) =>
           complete(service.previews(startDate, endDate))
         }
+      }
+    } ~ path("monthly" / "report" / "gen" / IntNumber) { date =>
+      get {
+        onComplete(service.gen(date)) {
+          case Success(value) => {
+            value._id match {
+              case Some(id@_) => complete(Map("id" -> id.stringify))
+              case None => complete(Map("id" -> ""))
+            }
+          }
+          case Failure(ex) => complete(s"生成月报出错: ${ex.getMessage}")
+        }
+      }
+    } ~ path("monthly" / "report" / IntNumber) { date =>
+      get {
+        import com.jeff.financing.dto.MonthlyReportRowJsonSupport._
+        complete(service.detail(date))
       }
     }
 
