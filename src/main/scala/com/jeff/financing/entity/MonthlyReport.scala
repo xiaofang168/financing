a959dc216cccb990fcef31461d42cc72fd3d5478
fangjie <fangjie@etouch.cn>
2021-01-07 18:13:46 +0800

    增加统计的基本信息

--- src/main/scala/com/jeff/financing/entity/MonthlyReport.scala
+++ src/main/scala/com/jeff/financing/entity/MonthlyReport.scala
@@ -7,15 +7,21 @@ import reactivemongo.api.bson.BSONObjectID
  * 平台及类别月报
  *
  * @param _id
- * @param date            日期
- * @param capital         本金
- * @param capitalInterest 本息
- * @param income          月收益
- * @param createTime      创建时间
+ * @param date               日期
+ * @param capitalSum         本金和
+ * @param capitalInterestSum 本息和
+ * @param incomeSum          收益和
+ * @param capital            本金
+ * @param capitalInterest    本息
+ * @param income             月收益
+ * @param createTime         创建时间
  */
 @Persistence(collName = "monthly_report")
 case class MonthlyReport(_id: Option[BSONObjectID],
                          date: Int,
+                         capitalSum: BigDecimal,
+                         capitalInterestSum: BigDecimal,
+                         incomeSum: BigDecimal,
                          capital: Capital,
                          capitalInterest: CapitalInterest,
                          income: Income,
