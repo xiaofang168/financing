4f994c93534fcfe947d0b9c9d73b23f3294bae84
fangjie <fangjie@etouch.cn>
2021-01-23 17:56:24 +0800

    显示创建时间字段

--- src/main/scala/com/jeff/financing/dto/StocktakingStats.scala
+++ src/main/scala/com/jeff/financing/dto/StocktakingStats.scala
@@ -2,4 +2,5 @@ package com.jeff.financing.dto
 
 case class StocktakingStats(_id: String,
                             date: Int,
-                            amount: BigDecimal)
+                            amount: BigDecimal,
+                            createTime: Long)
