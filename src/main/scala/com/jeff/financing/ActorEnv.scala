eaef2264e1b0f8e6241232f195cd180289efc675
fangjie <fangjie@etouch.cn>
2021-02-05 15:30:15 +0800

    使用zio启动akka http

---
+++ src/main/scala/com/jeff/financing/ActorEnv.scala
@@ -0,0 +1,27 @@
+package com.jeff.financing
+
+import akka.actor.ActorSystem
+import zio.Task
+
+trait ActorEnv {
+  def dependencies: ActorEnv.Service
+}
+
+object ActorEnv {
+
+  trait Service {
+    def getActorSystem: Task[ActorSystem]
+  }
+
+}
+
+trait ActorEnvLive extends ActorEnv {
+  // once instance, but not description. If wrapped in task will be evaluated each time (call by name)
+  private val system = ActorSystem("ZIO" + System.currentTimeMillis())
+
+  val dependencies = new ActorEnv.Service {
+    override def getActorSystem: Task[ActorSystem] = Task(system)
+  }
+}
+
+object ActorEnvLive extends ActorEnvLive
