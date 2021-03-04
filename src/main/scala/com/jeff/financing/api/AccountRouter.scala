dc23ae574d83718d028d49b7f496b493793206bb
fangjie <fangjie@etouch.cn>
2021-03-04 19:12:20 +0800

    使用Zlayer环境注入

--- src/main/scala/com/jeff/financing/api/AccountRouter.scala
+++ src/main/scala/com/jeff/financing/api/AccountRouter.scala
@@ -1,14 +1,18 @@
 package com.jeff.financing.api
 
 import akka.http.scaladsl.server.Directives._
+import com.jeff.financing.api.ZioSupport._
 import com.jeff.financing.entity.AccountJsonSupport._
-import com.jeff.financing.service.AccountService
+import com.jeff.financing.service.{AccountService, ZAccount}
 
 object AccountRouter extends AccountService {
 
   val route =
     path("accounts" / Remaining) { id =>
-      complete(get(id))
+      val value = ZAccount
+        .get(id) // the specification of the action
+        .provideLayer(ZAccount.live) // plugging in a real layer/implementation to run on
+      complete(value)
     }
 
 }
--- src/main/scala/com/jeff/financing/service/AccountService.scala
+++ src/main/scala/com/jeff/financing/service/AccountService.scala
@@ -3,8 +3,37 @@ package com.jeff.financing.service
 import com.jeff.financing.entity.Account
 import com.jeff.financing.repository.AccountRepository
 import com.jeff.financing.repository.PersistenceImplicits._
+import zio.{Has, Task, ZIO, ZLayer}
 
 import scala.concurrent.Future
+
+/**
+ * <pre>
+ * From my experience it is a real joy to write code with ZIO,
+ * especially in cases where you have complex concurrent/async requirements which are
+ * much simpler to implement with the features this library provides
+ * </pre>
+ */
+object ZAccount {
+
+  type ZAccountEnv = Has[ZAccount.Service]
+
+  trait Service {
+    def get(id: String): Task[Option[Account]]
+
+    def save(user: Account): Task[Boolean]
+  }
+
+  val live: ZLayer[Any, Nothing, ZAccountEnv] = ZLayer.succeed(new Service {
+    override def get(id: String): Task[Option[Account]] = ZIO.fromFuture(_ => AccountRepository.get(id))
+
+    override def save(user: Account): Task[Boolean] = ZIO.fromFuture(_ => AccountRepository.create(user))
+  })
+
+  def get(id: String): ZIO[ZAccountEnv, Throwable, Option[Account]] = ZIO.accessM(_.get.get(id))
+
+  def save(user: Account): ZIO[ZAccountEnv, Throwable, Boolean] = ZIO.accessM(_.get.save(user))
+}
 
 trait AccountService {
 
