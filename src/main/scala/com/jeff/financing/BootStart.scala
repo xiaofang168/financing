c1a1de0c8d883c4749143a04bee1af30016c6363
fangjie <fangjie@etouch.cn>
2021-04-04 22:45:51 +0800

    改为原来方式启动,zio启动占用太多cpu,mac风扇呼呼的响

--- src/main/scala/com/jeff/financing/BootStart.scala
+++ src/main/scala/com/jeff/financing/BootStart.scala
@@ -2,43 +2,26 @@ package com.jeff.financing
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.util.Timeout
 import ch.qos.logback.classic.LoggerContext
 import ch.qos.logback.classic.joran.JoranConfigurator
 import ch.qos.logback.core.util.StatusPrinter
 import org.slf4j.LoggerFactory
-import zio.{ExitCode, IO, Task, URIO, ZEnv, ZIO, console, _}
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
 import scala.language.postfixOps
 
 object BootStart extends App {
-
-  private val bindTask: ActorSystem => Task[Future[Http.ServerBinding]] = { system =>
-    implicit val sys: ActorSystem = system
-    implicit val executor: ExecutionContext = system.dispatcher
-    // setting logback
-    val lc = LoggerFactory.getILoggerFactory().asInstanceOf[LoggerContext]
-    val configurator = new JoranConfigurator()
-    configurator.setContext(lc)
-    lc.reset()
-    configurator.doConfigure(getClass.getResource("/logback.xml"))
-    StatusPrinter.printInCaseOfErrorsOrWarnings(lc)
-    Task(Http().newServerAt("localhost", 8088).bind(MainRouter.topLevelRoute))
-  }
-
-  val program: ZIO[ZEnv, Throwable, Future[Http.ServerBinding]] =
-    (for {
-      systemT <- ZIO.access[ActorEnv](_.dependencies.getActorSystem)
-      b <- systemT.flatMap { s => bindTask(s) }
-    } yield b
-      ).provide(ActorEnvLive)
-
-  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = program.foldM(
-    e => {
-      console.putStrLn(s"Server failed to start ${e.getMessage}") *> IO.fail(throw e)
-    },
-    _ => {
-      (console.putStrLn("Server started...") *> IO.succeed(0) *> IO.unit.forever).run.exitCode
-    }
-  )
+  implicit val timeout = Timeout(10 seconds)
+  implicit val system = ActorSystem("financing-system")
+  implicit val executor: ExecutionContext = system.dispatcher
+  // setting logback
+  val lc = LoggerFactory.getILoggerFactory().asInstanceOf[LoggerContext]
+  val configurator = new JoranConfigurator()
+  configurator.setContext(lc)
+  lc.reset()
+  configurator.doConfigure(getClass.getResource("/logback.xml"))
+  StatusPrinter.printInCaseOfErrorsOrWarnings(lc)
+  Http().newServerAt("localhost", 8088).bind(MainRouter.topLevelRoute)
 }
