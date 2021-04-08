aa9948b5f1863541f7a2fd07393bbdf687234e99
fangjie <fangjie@etouch.cn>
2021-04-08 15:48:46 +0800

    akka-http-circe使用

--- src/main/scala/com/jeff/financing/api/AccountRouter.scala
+++ src/main/scala/com/jeff/financing/api/AccountRouter.scala
@@ -7,12 +7,21 @@ import com.jeff.financing.service.ZAccount
 
 object AccountRouter {
 
+  private final case class Foo(bar: String)
+
+  private final case class Result[T](data: T)
+
   val route =
     path("accounts" / Remaining) { id =>
       val value = ZAccount
         .get(id) // the specification of the action
         .provideLayer(ZAccount.live) // plugging in a real layer/implementation to run on
       complete(value)
-    }
+    } ~
+      path("accounts") {
+        import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
+        import io.circe.generic.auto._
+        complete(Result(Foo("test")))
+      }
 
 }
