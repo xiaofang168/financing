1c5ea505d113dd8aa79be7d66fbc4489d24412ba
fangjie <fangjie@etouch.cn>
2021-06-30 22:59:12 +0800

    解决使用zio启动Server后cpu负载过高的问题，使用*> ZIO.never 代替 Foever

--- src/main/scala/com/jeff/financing/HttpServer.scala
+++ src/main/scala/com/jeff/financing/HttpServer.scala
@@ -9,30 +9,21 @@ import com.jeff.financing.ActorEnv.ActorEnv
 import org.slf4j.{Logger, LoggerFactory}
 import zio.{Task, ZIO, _}
 
+import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
-import scala.concurrent.{ExecutionContext, Future}
 import scala.language.postfixOps
 
 object HttpServer extends zio.App {
 
   private val logger: Logger = LoggerFactory.getLogger(HttpServer.getClass)
 
-  private val bindTask: ActorSystem => Task[Future[Http.ServerBinding]] = { system =>
+  private val bindTask: ActorSystem => Task[Http.ServerBinding] = { system =>
     implicit val timeout = Timeout(10 seconds)
     implicit val sys: ActorSystem = system
     implicit val executor: ExecutionContext = system.dispatcher
     val tt = Http().newServerAt("localhost", 8088).bind(MainRouter.topLevelRoute)
-    Task(tt)
+    ZIO.fromFuture(_ => tt)
   }
-
-  val program: ZIO[ZEnv, Throwable, Future[Http.ServerBinding]] =
-    (for {
-      _ <- Config.dbConfig.fold(f => logger.info(s"db failed to connect ${f.getMessage}"),
-        s => logger.info(s"db connected $s"))
-      systemT <- ZIO.accessM[ActorEnv](_.get.getActorSystem)
-      b <- bindTask(systemT)
-    } yield b
-      ).provideLayer(ActorEnv.live)
 
   override def run(args: List[String]): URIO[ZEnv, ExitCode] = {
     // setting logback
@@ -41,16 +32,21 @@ object HttpServer extends zio.App {
     configurator.setContext(lc)
     lc.reset()
     configurator.doConfigure(getClass.getResource("/logback.xml"))
-    program.foldM(
-      e => {
-        logger.info(s"server failed to start ${e.getMessage}")
-        IO.fail(throw e)
-      },
-      _ => {
-        logger.info("server started...")
-        IO.succeed(ExitCode.success).forever
-      }
-    )
+    (for {
+      _ <- Config.dbConfig.fold(f => logger.info(s"db failed to connect ${f.getMessage}"),
+        s => logger.info(s"db connected $s"))
+      systemT <- ZIO.accessM[ActorEnv](_.get.getActorSystem)
+      b <- bindTask(systemT).fold(
+        e => {
+          logger.info(s"server failed to start ${e.getMessage}")
+        },
+        _ => {
+          logger.info("server started...")
+        }
+      ) *> ZIO.never
+    } yield b
+      ).provideLayer(ActorEnv.live)
+       .exitCode
   }
 
 }
